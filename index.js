6b16e38e95eae0fabc932483c098b528035e2c09
Pooja <poojasankhala97@gmail.com>
2021-08-25 10:26:46 +0530

    pooja code merge

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+//Pooja Js - 1st
+let count = 0;
+let images = [];
+let time = 2000;
 
+images[0] = 'https://img6.hkrtcdn.com/15645/bnr_1564495_o.jpg';
+images[1] = 'https://img10.hkrtcdn.com/15640/bnr_1563949_o.jpg';
+images[2] = 'https://img6.hkrtcdn.com/15538/bnr_1553705_o.jpg';
+images[3] = 'https://img8.hkrtcdn.com/15641/bnr_1564057_o.jpg';
+images[4] = 'https://img8.hkrtcdn.com/14853/bnr_1485287_o.jpg';
+images[5] = 'https://img6.hkrtcdn.com/15632/bnr_1563185_o.png';
+function slidingShows() {
+  document.silde.src = images[count];
+  if (count < images.length - 1)
+  {
+    count++
+  }
+  else
+  {
+    count = 0;
+  }
+  setTimeout('slidingShows()', time)
+
+}
+window.onload = slidingShows;
+
+
+
+//Bhargav js -1st
     const medicine = [
         {
           image: "https://img8.hkrtcdn.com/12182/prd_1218147-GNC-Pro-Performance-Weight-Gainer-6.6-lb-Double-Chocolate_c_s.jpg",
@@ -47,7 +75,7 @@
 
 
         },
-        
+
     ]
 
     const medicine_json = JSON.stringify(medicine);
@@ -60,7 +88,7 @@
         let div_data = document.getElementById("data");
         med.forEach(function(products) {
             let div = document.createElement('div');
-            
+
             let m_name = document.createElement('p');
             m_name.innerText = products.name;
 
@@ -69,7 +97,7 @@
 
             let m_price2 = document.createElement('p');
             m_price2.innerText = products.price2;
-            
+
             let m_button = document.createElement('button');
             m_button.innerHTML = products.button;
 
