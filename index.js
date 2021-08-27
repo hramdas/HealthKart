32449842861497d03da40e5579e5d3238a753b14
Pooja <poojasankhala97@gmail.com>
2021-08-27 09:02:06 +0530

    Pooja Worked On CSS

--- index.js
+++ index.js
@@ -29,33 +29,21 @@ window.onload = slidingShows;
 //Bhargav js -1st
     const medicine = [
         {
-          image: "https://img8.hkrtcdn.com/12182/prd_1218147-GNC-Pro-Performance-Weight-Gainer-6.6-lb-Double-Chocolate_c_s.jpg",
+        image: "https://img4.hkrtcdn.com/14798/prd_1479773-HealthKart-HK-Vitals-Healthy-Joints-60-tablets_c_t.jpg",
           name : "MuscleBlaze Mass Gainer XXL, 11 lb Chocolate",
           Price1 : "Rs. 4535",
           price2 : "Rs 5399",
-          discount: "16%",
+        discount: "16% off",
           button: "Add",
           pricefinal : "Premium Price : 4324",
           rating : "4.6(1234)",
         },
         {
-          image: "https://img8.hkrtcdn.com/12182/prd_1218147-GNC-Pro-Performance-Weight-Gainer-6.6-lb-Double-Chocolate_c_s.jpg",
+          image: "https://img6.hkrtcdn.com/11003/prd_1100235-MuscleBlaze-Gold-Gainer-XXL-2.2-lb-Chocolate-Bliss_c_t.jpg",
           name : "MuscleBlaze Mass Gainer XXL, 11 lb Chocolate",
           Price1 : "Rs. 4535",
           price2 : "Rs 5399",
-          discount: "16%",
-          button: "Add",
-          pricefinal : "Premium Price : 4324",
-          rating : "4.6(1234)",
-
-
-        },
-        {
-          image: "https://img8.hkrtcdn.com/12182/prd_1218147-GNC-Pro-Performance-Weight-Gainer-6.6-lb-Double-Chocolate_c_s.jpg",
-          name : "MuscleBlaze Mass Gainer XXL, 11 lb Chocolate",
-          Price1 : "Rs. 4535",
-          price2 : "Rs 5399",
-          discount: "16%",
+          discount: "16% off",
           button: "Add",
           pricefinal : "Premium Price : 4324",
           rating : "4.6(1234)",
@@ -63,11 +51,23 @@ window.onload = slidingShows;
 
         },
         {
-          image: "https://img8.hkrtcdn.com/12182/prd_1218147-GNC-Pro-Performance-Weight-Gainer-6.6-lb-Double-Chocolate_c_s.jpg",
+          image: "https://img4.hkrtcdn.com/11965/prd_1196443-MuscleBlaze-High-Protein-Natural-Peanut-Butter-Unsweetened-0.750-kg-Crunchy_c_t.jpg",
           name : "MuscleBlaze Mass Gainer XXL, 11 lb Chocolate",
           Price1 : "Rs. 4535",
           price2 : "Rs 5399",
-          discount: "16%",
+          discount: "16% off",
+          button: "Add",
+          pricefinal : "Premium Price : 4324",
+          rating : "4.6(1234)",
+
+
+        },
+        {
+          image: "https://img10.hkrtcdn.com/15757/prd_1575689_c_t.jpg",
+          name : "MuscleBlaze Mass Gainer XXL, 11 lb Chocolate",
+          Price1 : "Rs. 4535",
+          price2 : "Rs 5399",
+          discount: "16% off",
           button: "Add",
           pricefinal : "Premium Price : 4324",
           rating : "4.6(1234)",
@@ -89,7 +89,7 @@ window.onload = slidingShows;
         med.forEach(function(products) {
             let div = document.createElement('div');
 
-            let m_name = document.createElement('p');
+          let m_name = document.createElement('h4');
             m_name.innerText = products.name;
 
             let m_price1 = document.createElement('p');
@@ -99,30 +99,30 @@ window.onload = slidingShows;
             m_price2.innerText = products.price2;
 
             let m_button = document.createElement('button');
-            m_button.innerHTML = "Add To Cart";
+            m_button.innerHTML = "Add";
             m_button.onclick = function (){
                 addtocart(products);
             };
 
-            let m_discount = document.createElement('p');
+          let m_discount = document.createElement('h5');
             m_discount.innerText = products.discount;
 
             let image = document.createElement('img');
             image.src = products.image;
 
-            let m_pricefinal = document.createElement('p');
+          let m_pricefinal = document.createElement('h6');
             m_pricefinal.innerText = products.pricefinal;
 
-            let m_rating = document.createElement('p');
-            m_rating.innerHTML = products.rating;
+          let m_rating = document.createElement('h3');
+          m_rating.innerHTML = '★' + '★' + '★' + '★' + products.rating;
 
-            div.append(image,m_discount,m_name, m_rating, m_price1, m_price2, m_button,m_pricefinal);
+            div.append(image,m_discount,m_name, m_rating, m_price1, m_button,m_pricefinal);
             div_data.append(div);
 
         });
     }
 
- 
+
     medProduct();
 
 
