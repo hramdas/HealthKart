d4a05adda0872aeda7088a23695c67cc0faf5d07
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-09-29 22:55:51 +0530

    Added flash products

--- controller/products.js
+++ controller/products.js
@@ -18,5 +18,53 @@ router.get("", async(req, res)=>{
    
 })
 
+router.get("/popular", async(req, res)=>{
+    const product = await Product.find().lean().exec();
+    product.sort((a,b)=>{
+        return b.rating - a.rating
+    })
+    return res.status(201).send({product})
+})
+
+router.get("/flashsale", async(req, res)=>{
+    const product = await Product.find().limit(10).lean().exec();
+    product.sort((a,b)=>{
+        return b.rating - a.rating
+    })
+    return res.status(201).send({product})
+})
+
+router.get("/priceL", async(req, res)=>{
+    const product = await Product.find().lean().exec();
+    product.sort((a,b)=>{
+        return a.price - b.price
+    })
+    return res.status(201).send({product})
+})
+
+router.get("/priceH", async(req, res)=>{
+    const product = await Product.find().lean().exec();
+    product.sort((a,b)=>{
+        return b.price - a.price
+    })
+    return res.status(201).send({product})
+})
+
+router.get("/discL", async(req, res)=>{
+    const product = await Product.find().lean().exec();
+    product.sort((a,b)=>{
+        return a.discount - b.discount
+    })
+    return res.status(201).send({product})
+})
+
+router.get("/discH", async(req, res)=>{
+    const product = await Product.find().lean().exec();
+    product.sort((a,b)=>{
+        return b.discount - a.discount
+    })
+    return res.status(201).send({product})
+})
+
 
 module.exports = router
