ddd6aa3a91768c14913d403cc04c61a22f13d272
Antony Chacko <antony101tony@gmail.com>
2021-09-29 20:44:55 +0530

    updated user schema

--- models/users.js
+++ models/users.js
@@ -1,11 +1,18 @@
 //user :- Name, Mobile No., Password
+// cart array and wishlist array also with user schema
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, require: true },
-    mobile: { type: Number, require: true },
-    password: { type: String, require: true }
+  name: { type: String, require: true },
+  mobile: { type: Number, require: true },
+  password: { type: String, require: true },
+  cart: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "products", require: false },
+  ],
+  wishlist: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "products", require: false },
+  ],
 });
 
 const User = mongoose.model("user", userSchema);
