405f0991b7c51de74a694da6cc81469f52c51b2f
Antony Chacko <antony101tony@gmail.com>
2021-10-03 19:13:04 +0530

    fixed cart count when user not logged in.

--- scripts/cartcount.js
+++ scripts/cartcount.js
@@ -1,24 +1,36 @@
 let user = JSON.parse(localStorage.getItem("HKuser"));
-let cart_data;
-console.log("ffffffffff");
-async function cartcounter() {
-  try {
-    console.log("dddddd");
-    let res = await fetch(`http://localhost:2200/users/${user}/cart/`);
-    let data = await res.json();
-    console.log(data);
-    let cart_data = data.items;
-    var len = cart_data.length;
-    console.log(len);
-    let spantext = document.createElement("span");
+if (user == null) {
+  //console.log("haaa");
+
+  setTimeout(() => {
     let txt = document.getElementById("cartOnNum");
-    txt.innerHTML = len;
+    txt.innerText = 0;
+  }, 500);
+} else {
+  let cart_data;
+  // console.log("ffffffffff");
+  async function cartcounter() {
+    try {
+      // console.log("dddddd");
+      if (user == null) {
+        return;
+      }
+      let res = await fetch(`http://localhost:2200/users/${user}/cart/`);
+      let data = await res.json();
+      // console.log(data);
+      let cart_data = data.items;
+      var len = cart_data.length;
+      //console.log(len);
+      let spantext = document.createElement("span");
+      let txt = document.getElementById("cartOnNum");
+      txt.innerHTML = len;
 
-    console.log(spantext);
+      //  console.log(spantext);
 
-    txt.appendChild(spantext);
-  } catch (error) {}
+      txt.appendChild(spantext);
+    } catch (error) {}
+  }
+  setInterval(() => {
+    cartcounter();
+  }, 100);
 }
-setInterval(() => {
-  cartcounter();
-}, 100);
