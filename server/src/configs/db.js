2848236db678a78cb20a650474c545a9b7bdab1a
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-02-09 18:35:17 +0530

    .vscode removed

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -2,5 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 module.exports = () => {
-  return mongoose.connect(process.env.DB_URL);
+  return mongoose.connect(
+    "mongodb+srv://hramdas:Ramdas1998@cluster0.or8t9.mongodb.net/HealthKart"
+  );
 };
